c2464a18c72463e8fdc5ea2e380713516f587d06
Marlon Monzon <moonrock650@gmail.com>
2016-11-30 15:50:26 +0800

    C practice using Eclipse C/C++

---
+++ c_practice/src/c_practice.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : c_practice.c
+ Author      : martianworm17
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
